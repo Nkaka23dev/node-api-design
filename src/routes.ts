4df92e176dbb62a8ed8dfdfc41953dda2b50f56e
ericnkaka <nkakaeric96@gmail.com>
2023-08-02 19:02:04 +0200

    feat: add route

---
+++ src/routes.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+
+const router = Router();
+
+// Products routes starts here
+
+router.get("/products", () => {});
+router.get("/products/:id", () => {});
+router.put("/products/:id", () => {});
+router.delete("/products", () => {});
+
+// Updates routes are heree
+
+router.get("/update", () => {});
+router.get("/update/:id", () => {});
+router.put("/update/:id", () => {});
+router.delete("/update", () => {});
+
+// Products routes starts here
+
+router.get("/updatepoint", () => {});
+router.get("/updatepoint/:id", () => {});
+router.put("/updatepoint/:id", () => {});
+router.delete("/updatepoint", () => {});

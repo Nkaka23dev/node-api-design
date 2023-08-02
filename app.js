83d09ac118c130871e1f37173a81f9595ac79344
ericnkaka <nkakaeric96@gmail.com>
2023-08-02 08:51:14 +0200

    feat: done with vanilla js

--- app.js
+++ app.js
@@ -1,34 +1,13 @@
-const http = require('http');
-const fs = require("fs");
+const http = require("http");
 
-const HandleIncomingAndOngoingRequest = (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    let path = "./views/";
-    switch (req.url) {
-        case "/":
-            res.statusCode = 200;
-            path += "index.html"
-            break
-        case "/about":
-            res.statusCode = 200;
-            path += "about.html"
-            break
-        default:
-            res.statusCode = 404;
-            path += "404.html"
+const server = http.createServer((req, res) => {
+    if (req.url === "/" && req.method === "GET") {
+        res.statusCode = 200;
+        console.log(res)
+        res.end();
     }
-    fs.readFile(path, (error, data) => {
-        if (error) {
-            console.log("Error Occured!");
-        } else {
-            res.write(data)
-            res.end();
-        }
-
-    })
-}
-const server = http.createServer(HandleIncomingAndOngoingRequest);
+});
 
 server.listen(4200, () => {
-    console.log("Server is Created and is running on port 4200",);
-})
+   console.log("Server is learning on PORT http://localhost:4200");
+}) 
